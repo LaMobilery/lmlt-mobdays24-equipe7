380cb098dd710668253cf60cb9da41b4721845a6
Romain-Brement <rbrement@keyneosoft.fr>
2024-06-12 14:15:18 +0200

    feat(home): add title adjust logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const logoSize = document.body.clientWidth / 4;
+
   return (
     <main className={styles.main}>
-      <div className={styles.logo}>
-        <Image src="/logo.png" width={492} height={492} alt="logo wizard" />
+      <div className={styles.mainBlock}>
+        <div className={styles.logo} style={{ marginTop: logoSize / 1.6 }}>
+          <Image src="/logo.png" width={logoSize} height={logoSize} alt="logo wizard" />
+        </div>
+        <h1 className={styles.title}>CVWizard</h1>
+        <p>Wingardium Leviosaaaaaa</p>
       </div>
       <div className={styles.questionBlock}>
         <h2>C'est la première question ?</h2>
