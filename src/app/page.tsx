c4bfcb73b0aae88a73479b252f7df1e1883e998f
Romain-Brement <rbrement@keyneosoft.fr>
2024-06-12 13:03:40 +0200

    feat(home): add background and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.logo}>
+        <Image src="/logo.png" width={492} height={492} alt="logo wizard" />
+      </div>
+      <div>
+        <h2>C'est la première question</h2>
+        <input type="text" name="test" />
+      </div>
+    </main>
+  );
 }
